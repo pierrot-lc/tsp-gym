d23d6aca758db044d9f775338ba6822fecda024f
Pierrot LC <pierrotlc@proton.me>
2024-02-03 18:44:20 +0000

    ✨ tsp: distances and edge sampling

--- src/env/test_env.py
+++ src/env/test_env.py
@@ -1,25 +1,74 @@
+from itertools import product
+
 import pytest
 import torch
 
-from .tsp import random_instances
+from .tsp import compute_distances, random_instances, sample_edges
+
+generator = torch.Generator(device="cpu")
+generator.manual_seed(0)
+
+
+
+@pytest.mark.parametrize(
+    "instances, lambda_",
+    [
+        (random_instances(5, 10, generator), 1),
+        (random_instances(5, 10, generator), 1),
+        (random_instances(5, 10, generator), 1),
+        (random_instances(5, 10, generator), 1),
+    ],
+)
+def test_sample_edges(instances: torch.Tensor, lambda_: float):
+    """Make sure each city is at least connected to its closest neighbour."""
+    distances = compute_distances(instances)
+    edges = sample_edges(distances, lambda_, generator)
+
+    for edges_, distances_ in zip(edges, distances):
+        for city_id in range(len(distances_)):
+            distances_[city_id, city_id] = float("+inf")
+            closest_city_id = torch.argmin(distances_[city_id])
+            assert edges_[city_id, closest_city_id] == 1
+
+@pytest.mark.parametrize(
+    "instances",
+    [
+        random_instances(5, 10, generator),
+        random_instances(5, 10, generator),
+        random_instances(5, 10, generator),
+        random_instances(5, 10, generator),
+    ],
+)
+def test_compute_distances(instances: torch.Tensor):
+    n_instances, n_cities, _ = instances.shape
+    distances = compute_distances(instances)
+
+    assert distances.shape == torch.Size([n_instances, n_cities, n_cities])
+
+    ground_truth_distances = torch.zeros_like(distances)
+    for instance_id, (instance, dists) in enumerate(zip(instances, distances)):
+        for city_1, city_2 in product(range(n_cities), range(n_cities)):
+            coords_1, coords_2 = instance[city_1], instance[city_2]
+            dist = ((coords_1 - coords_2) ** 2).sum().sqrt()
+            ground_truth_distances[instance_id, city_1, city_2] = dist
+
+    assert torch.allclose(ground_truth_distances, distances)
 
 
 @pytest.mark.parametrize(
     "n_cities, n_instances, x_lim, y_lim",
     [
-        (10, 100, (0, 1), (0, 1)),
-        (10, 100, (0, 10), (0, 10)),
-        (10, 100, (-10, 10), (-10, 10)),
+        (100, 10, (0, 1), (0, 1)),
+        (100, 10, (0, 10), (0, 10)),
+        (100, 10, (-10, 10), (-10, 10)),
         (10, 100, (-10, 10), (0, 1)),
         (10, 100, (0, 1), (-10, 10)),
     ],
 )
 def test_random_instances(
-    n_cities: int, n_instances: int, x_lim: tuple[int, int], y_lim: tuple[int, int]
+    n_instances: int, n_cities: int, x_lim: tuple[int, int], y_lim: tuple[int, int]
 ):
-    generator = torch.Generator(device="cpu")
-    generator.manual_seed(0)
-    instances = random_instances(n_cities, n_instances, generator, x_lim, y_lim)
+    instances = random_instances(n_instances, n_cities, generator, x_lim, y_lim)
 
     assert instances.shape == torch.Size([n_instances, n_cities, 2])
 
--- src/env/tsp.py
+++ src/env/tsp.py
@@ -1,9 +1,67 @@
 import torch
 
 
+def sample_edges(
+    distances: torch.Tensor, lambda_: float, generator: torch.Generator
+) -> torch.Tensor:
+    """Sample edges based on the distance between each cities.
+    The closer two cities are, the more likely an edge will exist.
+    Each city is at least connected to its closest city.
+
+    The probabilities are computed based on the exponential distribution.
+
+    ---
+    Args:
+        distances: The distances between each city.
+            Shape of [n_instances, n_cities, n_cities].
+        lambda_: The lambda parameter of the exponential distribution.
+        generator: The random number generator.
+
+    ---
+    Returns:
+        The adjacency matrix.
+            Shape of [n_instances, n_cities, n_cities].
+    """
+    device = distances.device
+    arange = torch.arange(distances.shape[1], device=device)
+
+    # Sample the edges.
+    probabilities = (1 / lambda_) * torch.exp(-distances / lambda_)
+    edges = torch.rand(probabilities.shape, generator=generator) <= probabilities
+    edges = edges.int()
+
+    # Remove the self-loops.
+    edges[:, arange, arange] = 0
+
+    # Get the closest city.
+    distances = distances.clone()
+    distances[:, arange, arange] = float("+inf")
+    closest_cities = torch.argmin(distances, dim=2)
+
+    # Connect each city to its closest city.
+    edges.scatter_(2, closest_cities.unsqueeze(2), 1)
+
+    return edges
+
+def compute_distances(instances: torch.Tensor) -> torch.Tensor:
+    """Compute the distances between each city.
+
+    ---
+    Args:
+        instances: The TSP instances.
+            Shape of [n_instances, n_cities, 2].
+
+    ---
+    Returns:
+        The distance matrix.
+            Shape of [n_instances, n_cities, n_cities].
+    """
+    return torch.cdist(instances, instances, p=2)
+
+
 def random_instances(
-    n_cities: int,
     n_instances: int,
+    n_cities: int,
     generator: torch.Generator,
     x_lim: tuple[int, int] = (0, 1),
     y_lim: [int, int] = (0, 1),
@@ -15,6 +73,11 @@ def random_instances(
         The TSP instances.
             Shape of [n_instances, n_cities, 2].
     """
+    assert n_instances > 0
+    assert n_cities > 1
+    assert x_lim[0] <= x_lim[1]
+    assert y_lim[0] <= y_lim[1]
+
     x = torch.rand(
         (n_instances, n_cities, 1),
         generator=generator,
