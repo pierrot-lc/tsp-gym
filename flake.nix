73676ad06dd415a476774f1d4edb925accaa797d
Pierre Pereira <pstmrtem@outlook.com>
2024-02-15 07:09:27 +0100

    ➖ dependencies: removed unusued shell.nix

--- flake.nix
+++ flake.nix
@@ -25,31 +25,7 @@
         allowUnfree = true;
         cudaSupport = true;
       };
-
-      # Override all torch packages for `torch-bin`.
-      # This is important since the other torch derivatives
-      # need a default torch package and assume it's `torch`.
-
-      # However, this is not necessary. By setting `cudaSupport = true`,
-      # the `torch` package and its derivatives will be built with cuda
-      # support automatically. It makes things easier.
-
-      # Taken from: https://discourse.nixos.org/t/overriding-torch-with-torch-bin-for-all-packages/37086/2.
-      # overlays = [
-      #   (final: prev: {
-      #     pythonPackagesExtensions = [
-      #       (py-final: py-prev: {
-      #         torch = py-final.torch-bin;
-      #       })
-      #     ];
-      #   })
-      # ];
     };
-
-
-    # To avoid compiling the `torch` package, `einops` and other cuda
-    # intensive package, you have to use the cachix from the
-    # cuda-maintainers.
 
     python-packages = ps: with ps; [
       pip
@@ -65,7 +41,7 @@
       pytest
       torch
       torchinfo
-      torchrl  # Does not exists on 23.11.
+      torchrl
       tqdm
       wandb
     ];
